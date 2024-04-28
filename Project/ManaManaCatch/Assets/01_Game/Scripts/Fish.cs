940721b78fcc23265543b4db199ce02ecc6c4eff
YoshinoNao <113161290+Yoshino-Nao@users.noreply.github.com>
2024-04-28 16:07:08 +0900

    resouseファイル削除+Resourcesファイル内のFishファイル内にいらすとや魚画像追加、FIsh.csHitBox.cs追加

---
+++ Project/ManaManaCatch/Assets/01_Game/Scripts/Fish.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    [SerializeField] private HitBox m_hitbox;
+    [SerializeField] private float m_timing = 3f;
+    [SerializeField] private float m_elapsedTime = 0f;
+    [SerializeField] private bool m_isStart = false;
+    [SerializeField] private float m_interval = 0.1f;
+
+    private Transform m_tf = null;
+    private Vector3 m_startPos = Vector3.zero;
+    private Vector3 m_endPos = Vector3.zero;
+    private float m_canScoopTime = 0.5f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        m_tf = transform;
+        m_startPos = m_tf.position;
+        m_endPos = m_hitbox.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+
+            if (EvaluateTiming(m_elapsedTime, m_timing))
+            {
+                Debug.Log("Hit");
+            }
+            else
+            {
+                Debug.Log("Miss");
+            }
+        }
+        Debug.Log(CanScoop());
+        //経過時間+判定
+        if (m_elapsedTime > m_timing + m_interval)
+        {
+
+        }
+        //スタートフラグをtrueにしないと移動、経過時間の計測をしない
+        if (m_isStart)
+        {
+            m_elapsedTime += Time.deltaTime;
+            m_tf.position = Vector3.LerpUnclamped(m_startPos, m_endPos, m_elapsedTime / m_timing);
+        }
+    }
+    /// <summary>
+    /// タイミングの評価
+    /// </summary>
+    /// <param 経過時間="elapsedTime"></param>
+    /// <param タイミング="timing"></param>
+    /// <returns>経過時間とタイミングの差がm_interval以内であればtrue</returns>
+    private bool EvaluateTiming(float elapsedTime, float timing)
+    {
+        return Mathf.Abs(elapsedTime - timing) <= m_interval;
+
+    }
+    public] bool CanScoop()
+    {
+        return Mathf.Abs(m_elapsedTime - m_timing) <= m_canScoopTime;
+    }
+}
---
+++ Project/ManaManaCatch/Assets/01_Game/Scripts/HitBox.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HitBox : MonoBehaviour
+{
+    [SerializeField] private float m_raycastLength = 1;
+    private SpriteRenderer m_spriteRenderer = null;
+    private Transform m_tf = null;
+    private LayerMask m_layerMask;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_tf = transform;
+        m_spriteRenderer = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+
+}
